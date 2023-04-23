635dac4413e7950adfdbb7234ac811c35431ff20
saignan <saignankarrolla695@gmail.com>
2023-04-23 22:47:15 +0530

    Updated

--- Generic_Test_Maximum/UC1.cs
+++ Generic_Test_Maximum/UC1.cs
@@ -8,5 +8,25 @@ namespace Generic_Test_Maximum
 {
     internal class UC1
     {
+        public int maximumValue(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && (firstvalue.CompareTo(thirdvalue) > 0))
+            {
+                return firstvalue;
+            }
+            else if (secondvalue.CompareTo(firstvalue) > 0 && (secondvalue.CompareTo(thirdvalue) > 0))
+            {
+                return secondvalue;
+            }
+            else if (thirdvalue.CompareTo(firstvalue) > 0 && (thirdvalue.CompareTo(secondvalue) > 0))
+            {
+                return thirdvalue;
+            }
+            else
+            {
+                return 0; ;
+            }
+
+        }
     }
 }
