68719dbdee9e713ab760ae326de22af73f702d77
saignan <saignankarrolla695@gmail.com>
2023-04-23 22:21:06 +0530

    Non Generic Implementation

---
+++ Generic_Test_Maximum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_Test_Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
